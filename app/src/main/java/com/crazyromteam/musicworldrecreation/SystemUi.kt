1ddff16915012630ebfc2e244a4496dbe1677928
Eridion Kiorai <erikiorai@gmail.com>
2022-04-03 22:02:22 +0300

    fix fullscreen on bellow android 10

--- app/src/main/java/com/crazyromteam/musicworldrecreation/SystemUi.kt
+++ app/src/main/java/com/crazyromteam/musicworldrecreation/SystemUi.kt
@@ -4,6 +4,7 @@ import android.R
 import android.os.Build
 import android.view.View
 import android.view.Window
+import android.view.WindowManager
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -11,22 +12,20 @@ import androidx.core.view.WindowInsetsControllerCompat
 class SystemUi {
     fun hideSystemUI(window: Window) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-            WindowInsetsControllerCompat(
-                window,
-                window.decorView.findViewById(R.id.content)
-            ).let { controller ->
-                controller.hide(WindowInsetsCompat.Type.systemBars())
+                WindowCompat.setDecorFitsSystemWindows(window, false)
+                WindowInsetsControllerCompat(
+                    window,
+                    window.decorView.findViewById(R.id.content)
+                ).let { controller ->
+                    controller.hide(WindowInsetsCompat.Type.systemBars())
 
-                controller.systemBarsBehavior =
-                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                    controller.systemBarsBehavior =
+                        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+            }
+            else {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
             }
         }
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
-        else {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        }
     }
-}
