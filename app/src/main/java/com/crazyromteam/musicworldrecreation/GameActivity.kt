bc00468af47393d5d727d0541bb1afc51e1869a8
Eridion Kiorai <erikiorai@gmail.com>
2022-04-03 14:03:51 +0300

    added alert dialog before exiting and now TitleActivity starts for scratch

--- app/src/main/java/com/crazyromteam/musicworldrecreation/GameActivity.kt
+++ app/src/main/java/com/crazyromteam/musicworldrecreation/GameActivity.kt
@@ -1,18 +1,15 @@
 package com.crazyromteam.musicworldrecreation
 
 import android.app.Activity
+import android.content.Intent
 import android.media.MediaPlayer
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 
 class GameActivity : Activity() {
     var gamesound = null as MediaPlayer?
-    var mTitleActivity = null as TitleActivity?
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        mTitleActivity = TitleActivity()
         SystemUi().hideSystemUI(window)
         gamesound = MediaPlayer.create(this.applicationContext, R.raw.morning_dew)
         gamesound?.start()
@@ -21,6 +18,8 @@ class GameActivity : Activity() {
     override fun onBackPressed() {
         super.onBackPressed()
         gamesound?.stop()
-        mTitleActivity?.titlesound?.start()
+        val titleactivity = Intent(this, TitleActivity::class.java)
+        startActivity(titleactivity)
+        finish()
     }
 }
--- app/src/main/java/com/crazyromteam/musicworldrecreation/TitleActivity.kt
+++ app/src/main/java/com/crazyromteam/musicworldrecreation/TitleActivity.kt
@@ -1,10 +1,10 @@
 package com.crazyromteam.musicworldrecreation
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.media.MediaPlayer
-import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
@@ -36,12 +36,28 @@ class TitleActivity : Activity() {
         titletext.startAnimation(anim)
     }
 
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(resources.getString(R.string.exit_music_world))
+        builder.setMessage(resources.getString(R.string.exit_music_world_confirmation))
+            .setCancelable(false)
+            .setPositiveButton(
+                resources.getString(R.string.yes)
+            ) { dialog, id -> this@TitleActivity.finish() }
+            .setNegativeButton(
+                resources.getString(R.string.no)
+            ) { dialog, id -> dialog.cancel() }
+        val alert = builder.create()
+        alert.show()
+    }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_DOWN) {
             if ((event.action != MotionEvent.ACTION_MOVE)) {
                 titlesound?.stop()
                 val gameActivity = Intent(this, GameActivity::class.java)
                 startActivity(gameActivity)
+                finish()
                 return true
             }
         }
